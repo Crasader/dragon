63ed8873c8d2e60396f57bbd1137308bd483fe31
xingchong <xingchong6@163.com>
2015-12-08 21:02:48 +0800

    构思代码结构。

--- dragon/Classes/scene/LoadingScene.h
+++ dragon/Classes/scene/LoadingScene.h
@@ -10,5 +10,11 @@
 #define __dragon__LoadingScene__
 
 #include <stdio.h>
+#include "cocos2d.h"
+
+class LoadingScene : public cocos2d::Scene
+{
+    
+};
 
 #endif /* defined(__dragon__LoadingScene__) */
