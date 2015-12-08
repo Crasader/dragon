2c58a156af7fb23f29bff5dc06a1d4d8da7bfcf0
xingchong <xingchong6@163.com>
2015-12-08 21:02:08 +0800

    构思代码结构。

---
+++ dragon/Classes/scene/LoadingScene.cpp
@@ -0,0 +1,9 @@
+//
+//  LoadingScene.cpp
+//  dragon
+//
+//  Created by xingchong on 12/8/15.
+//
+//
+
+#include "LoadingScene.h"
---
+++ dragon/Classes/scene/LoadingScene.h
@@ -0,0 +1,14 @@
+//
+//  LoadingScene.h
+//  dragon
+//
+//  Created by xingchong on 12/8/15.
+//
+//
+
+#ifndef __dragon__LoadingScene__
+#define __dragon__LoadingScene__
+
+#include <stdio.h>
+
+#endif /* defined(__dragon__LoadingScene__) */
