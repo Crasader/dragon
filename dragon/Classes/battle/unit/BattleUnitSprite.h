52a57d4e1f79fe5c74857b13f6db91110917393e
xingchong <xingchong6@163.com>
2016-02-27 17:22:28 +0800

    战场内基类。

--- dragon/Classes/battle/unit/BattleUnitSprite.h
+++ dragon/Classes/battle/unit/BattleUnitSprite.h
@@ -10,5 +10,24 @@
 #define __dragon__BattleUnitSprite__
 
 #include <stdio.h>
+#include "MapSprite.h"
+
+class BattleUnitSprite : public MapSprite
+{
+public:
+    BattleUnitSprite();
+    ~BattleUnitSprite();
+    
+public:
+    virtual bool init() override;
+    virtual void destroy() override;
+    virtual void initData(int guid, int configId);
+    virtual void initView();
+    
+private:
+    ValueMap _dataDic;
+    std::function<void()> _onLandCallback;
+    std::function<void()> _offLandCallback;
+};
 
 #endif /* defined(__dragon__BattleUnitSprite__) */
--- dragon/Classes/battle/unit/MapSprite.h
+++ dragon/Classes/battle/unit/MapSprite.h
@@ -22,17 +22,37 @@ public:
     virtual bool init() override;
     virtual void destroy() override;
     virtual void reset();
+    virtual void setActionMovePt(ValueMap param);
+    virtual void setMoveSpeed(Vec2 movePt, Vec2 moveAddPt);
+    virtual void playEffect();
+    virtual void playBeHitEffect(int effectId);
+    virtual void playEffectWithId(int effectId);
+    virtual void update(float dt);
+    virtual void setCollideRange(Size rect) override;
     
     void showRectColor();
     void playRepeatForever(int actionId);
     void playOnce(int actionId);
-    
+    void playAction(int actionId);
+    void handlerActionData(ValueMap actionConfig);
+    bool isPause();
+    void psuseAction();
+    void resumeAction();
+    bool isToRight();
+    void setToRight(bool toRight);
+    bool getIsFlipX();
+    void setFlipx(bool isFlip);
+    bool isOnLand();
+    void onFinishAniCallback(float actionId, std::string actionName);
+    void onBreakAniCallback(ValueMap config);
+    void onRemoveEffectAni();
+    void resetAniCallback();
     
 private:
     int        _spriteId;
     float      _actionLastTime;
     float      _actionWaitTime;
-    ValueMap   _curAniConfig;
+    ValueMap   _curActionConfig;
     Node       _actionAni;
     Node       _actionEffect;
     LayerColor _RectColor;
